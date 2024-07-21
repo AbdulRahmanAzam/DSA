5c5aef664014fa3ff13efe786bbd7ccde3c56672
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2024-07-21 08:35:24 +0500

    Update Tree.cpp

--- Trees/Tree.cpp
+++ Trees/Tree.cpp
@@ -14,6 +14,8 @@ class node{
   }
 };
 
+
+
 node* buildTree(node* root){
   int data;
   cout<<"Enter data: "<<endl;
@@ -32,6 +34,45 @@ node* buildTree(node* root){
 
   return root;
 }
+
+
+
+void buildFromLevelOrder(node* &root){
+  queue<node*> que;
+  int data;
+  cout << "Enter data for root: " << endl;
+  cin >> data;
+
+  root = new node(data);
+  que.push(root);
+
+  while(!que.empty()){
+    node* temp = que.front();
+    que.pop();
+
+    cout << "Enter left node for " << temp->data << endl;
+    int left;
+    cin >> left;
+
+    if(left != -1){
+      temp->left = new node(left);
+      que.push(temp->left); 
+    }
+
+    cout << "Enter right node for " << temp->data << endl;
+    int right;
+    cin >> right;
+
+    if(right != -1){
+      temp->right = new node(right);
+      que.push(temp->right);
+    }
+  }
+}
+
+
+
+
 // 1 2 3 -1 -1 -1 3 5 -1 -1 6 -1 -1
 int main() {
   node* root = nullptr;
