073119ebdda0482ba86edd77e6a69f86ea99c79a
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2024-09-08 15:48:42 +0500

    Update task 2.cpp

--- Z lab 3/task 2.cpp
+++ Z lab 3/task 2.cpp
@@ -22,77 +22,18 @@ class SinglyLinkedList {
             head = new Node(data, head);
         }
 
-        void insertAtEnd(int data) {
-            Node* newNode = new Node(data);
-            if(!head) {
-                head = newNode;
-            } else {
-                Node* curr = head;
-                while(curr->next) {
-                    curr = curr->next;
-                }
-                curr->next = newNode;
-            }
+        Node* reverseHelper(Node* node) {
+            if(!node || !node->next) return node;
+
+            Node* reversed = reverseHelper(node->next);
+            node->next->next = node;
+            node->next = nullptr;
+
+            return reversed;
         }
 
-        void insertBeforedataue(int targetdata, int insertdata) {
-            if(head->data == targetdata) {
-                head = new Node(insertdata, head);
-            } else {
-                Node* curr = head;
-                while(curr->next->data != targetdata) {
-                    curr = curr->next;
-                }
-                curr->next = new Node(insertdata, curr->next);
-            }
-        }
-
-        void insertAfterdataue(int targetdata, int insertdata) {
-            Node* curr = head;
-            while(curr->data != targetdata) {
-                curr = curr->next;
-            }
-            curr->next = new Node(insertdata, curr->next);
-        }
-
-        void deleteAtFront() {
-            Node* temp = head;
-            head = head->next;
-            delete temp;
-        }
-
-        void deleteAtEnd() {
-            Node* curr = head;
-            while(curr->next->next) {
-                curr = curr->next;
-            }
-
-            Node* temp = curr->next;
-            curr->next = nullptr;
-            delete temp;
-        }
-
-        void deleteParticulardataue(int data) {
-            if(data == head->data) {
-                deleteAtFront();
-            } else {
-                Node* curr = head;
-                while(curr->next->data != data) {
-                    curr = curr->next;
-                }
-
-                Node* temp = curr->next;
-                curr->next = curr->next->next;
-                delete temp;
-            }
-        }
-
-        int countNodes() {
-            int count = 0;
-            for(Node* curr = head; curr; curr = curr->next) {
-                ++count;
-            }
-            return count;
+        void reverse() {
+            head = reverseHelper(head);
         }
 
         void printList() {
@@ -117,28 +58,18 @@ int main() {
     sll.insertAtFront(2);
     sll.insertAtFront(3);
     sll.insertAtFront(4);
+    sll.insertAtFront(5);
+    sll.insertAtFront(6);
+    sll.insertAtFront(7);
+    sll.insertAtFront(8);
 
+    cout << "Before reversing" << endl;
     sll.printList();
 
-    sll.insertAtEnd(5);
-    sll.insertAtEnd(6);
-    sll.insertAtEnd(7);
-    sll.insertAtEnd(8);
+    sll.reverse();
 
+    cout << endl << "After reversing" << endl;
     sll.printList();
-
-    sll.insertBeforedataue(5, 9);
-    sll.insertAfterdataue(2, 10);
-
-    sll.printList();
-
-    sll.deleteAtFront();
-    sll.deleteAtEnd();
-    sll.deleteParticulardataue(1);
-
-    sll.printList();
-
-    cout << endl << "Number of nodes in list : " << sll.countNodes() << endl;
 
     return 0;
 }
