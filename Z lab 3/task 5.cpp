b6f72fcce3d2d2d5286a4d1387819af275a15e96
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2024-09-30 08:43:26 +0500

    Update task 5.cpp

--- Z lab 3/task 5.cpp
+++ Z lab 3/task 5.cpp
@@ -16,18 +16,66 @@ class DoublyLinkedList{
 
     DoublyLinkedList() : head(nullptr){}
 
-    void sort(){
-        if (head == nullptr) return;
+// Sort by  values
+    // void sort(){
+    //     if (head == nullptr) return;
 
-        for(Node* temp = head; temp; temp = temp->next){
+    //     for(Node* temp = head; temp; temp = temp->next){
 
-            Node* current = temp;
-            while(current != nullptr){
-                if(temp->data > current->data){
-                    swap(temp->data, current->data);
-                }
+    //         Node* current = temp;
+    //         while(current != nullptr){
+    //             if(temp->data > current->data){
+    //                 swap(temp->data, current->data);
+    //             }
+    //             current = current->next;
+    //         }
+    //     }
+    // }
+
+
+// sort by nodes
+void sort() {
+        Node* last_sorted = nullptr;
+        Node* current = head;
+        if (not current or not current->next) return;
+
+        while (current) {
+            Node* min_node = find_min(current);
+            if (min_node == current) {
+                last_sorted = current;
                 current = current->next;
+                continue;
             }
+
+            if (min_node == tail)
+                tail = min_node->prev;
+            if (min_node->prev)
+                min_node->prev->next = min_node->next;
+            if (min_node->next)
+                min_node->next->prev = min_node->prev;
+            min_node->next = current;
+            min_node->prev = last_sorted;
+            if (last_sorted)
+                last_sorted->next = min_node;
+            else
+                last_sorted = min_node, head = min_node;
+            current->prev = min_node;
+            last_sorted = min_node;
+        }
+
+        // removing duplicates
+        current = head;
+        while (current->next) {
+            if (current->value == current->next->value) {
+                Node* temp = current->next;
+                if (temp == tail)
+                    tail = current;
+                current->next = current->next->next;
+                if (current->next)
+                    current->next->prev = current;
+                delete temp;
+            } else
+                current = current->next;
         }
     }
 
